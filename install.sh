45b18e89837a9d46199bc15d737c78500916b40f
anik3tra0 <anik3tra0@gmail.com>
2017-10-14 16:45:08 +0530

    Fixed bugs leading to crash after installing rbenv and more of the same and fixed bugs again

--- install.sh
+++ install.sh
@@ -10,7 +10,7 @@ set -e
 
 sudo -S apt-get update < ~/iambatman.txt;
 
-sudo apt-get install -y git-core curl zlib1g-dev build-essential libssl-dev; libreadline-dev libyaml-dev libsqlite3-dev sqlite3 libxml2-dev libxslt1-dev; libcurl4-openssl-dev python-software-properties libffi-dev nodejs;
+sudo apt-get install -y git-core curl zlib1g-dev build-essential libssl-dev libreadline-dev libyaml-dev libsqlite3-dev sqlite3 libxml2-dev libxslt1-dev libcurl4-openssl-dev python-software-properties libffi-dev nodejs;
 
 cd ~;
 sudo -S apt-get update < ~/iambatman.txt;
