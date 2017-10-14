8efd72d55ee23bb85309fa26ba36031b16110cb1
anik3tra0 <anik3tra0@gmail.com>
2017-10-14 16:41:24 +0530

    Fixed bugs leading to crash after installing rbenv

--- install.sh
+++ install.sh
@@ -1,40 +1,45 @@
 #!/bin/bash
 set -e
-if [[ $EUID -ne 0 ]]; then
-	echo "This script must be run as root, use sudo "$0" instead" 1>&2
-	exit 1
-fi
-echo "WE ARE ROOT!"
 
-sudo apt-get update
+# if [[ $EUID -ne 0 ]]; then
+# 	echo "This script must be run as root, use sudo "$0" instead" 1>&2
+# 	exit 1
+# fi
+#
+# echo "WE ARE ROOT!"
 
-sudo apt-get install -y git-core curl zlib1g-dev build-essential libssl-dev libreadline-dev libyaml-dev libsqlite3-dev sqlite3 libxml2-dev libxslt1-dev libcurl4-openssl-dev python-software-properties libffi-dev nodejs
+sudo -S apt-get update < ~/iambatman.txt;
 
-cd ~
+sudo apt-get install -y git-core curl zlib1g-dev build-essential libssl-dev; libreadline-dev libyaml-dev libsqlite3-dev sqlite3 libxml2-dev libxslt1-dev; libcurl4-openssl-dev python-software-properties libffi-dev nodejs;
 
-git clone https://github.com/rbenv/rbenv.git ~/.rbenv
-echo 'export PATH="$HOME/.rbenv/bin:$PATH"' >> ~/.bashrc
-echo 'eval "$(rbenv init -)"' >> ~/.bashrc
-exec $SHELL
+cd ~;
 
-git clone https://github.com/rbenv/ruby-build.git ~/.rbenv/plugins/ruby-build
-echo 'export PATH="$HOME/.rbenv/plugins/ruby-build/bin:$PATH"' >> ~/.bashrc
-exec $SHELL
+git clone https://github.com/rbenv/rbenv.git ~/.rbenv;
+echo 'export PATH="$HOME/.rbenv/bin:$PATH"' >> ~/.bashrc;
+echo 'eval "$(rbenv init -)"' >> ~/.bashrc;
+exec $SHELL;
 
-rbenv install 2.3.1
-rbenv global 2.3.1
-ruby -v
+git clone https://github.com/rbenv/ruby-build.git ~/.rbenv/plugins/ruby-build;
+echo 'export PATH="$HOME/.rbenv/plugins/ruby-build/bin:$PATH"' >> ~/.bashrc;
+exec $SHELL;
 
-sudo apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv-keys 561F9B9CAC40B2F7
-sudo apt-get install -y apt-transport-https ca-certificates
+rbenv install 2.3.1;
+rbenv global 2.3.1;
+ruby -v;
+
+sudo -S apt-get update < ~/iambatman.txt;
+sudo apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv-keys 561F9B9CAC40B2F7;
+sudo apt-get install -y apt-transport-https ca-certificates;
 
 # Add Passenger APT repository
-sudo sh -c 'echo deb https://oss-binaries.phusionpassenger.com/apt/passenger xenial main > /etc/apt/sources.list.d/passenger.list'
-sudo apt-get update
+sudo sh -c 'echo deb https://oss-binaries.phusionpassenger.com/apt/passenger xenial main > /etc/apt/sources.list.d/passenger.list';
+sudo -S apt-get update < ~/iambatman.txt;
 
 # Install Passenger & Nginx
-sudo apt-get install -y nginx-extras passenger
+sudo -S apt-get update < ~/iambatman.txt;
 
-sudo service nginx start
+sudo apt-get install -y nginx-extras passenger;
 
-sudo apt-get install -y postgresql postgresql-contrib libpq-dev
+sudo service nginx start;
+
+sudo apt-get install -y postgresql postgresql-contrib libpq-dev;
